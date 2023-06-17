cea3d2c0c62d5f640f5355eda8ec8e849de717e2
jpblicer <jpblicer@gmail.com>
2023-06-17 20:55:30 +0900

    Created Kana class with cunstructor for Moji and sound. Added test method to ensure that it is working correctly. created several variables to store Hiragana objects with sound and moji. ran test to ensure it was set up correctly

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-console.log("test")
+/*
+TODO
+
+DONE Create a Kana class
+DONE Create a Hiragana object [start with a i u e o]
+    DONE Should have moji and the sound
+
+
+Quiz will display to page
+    Quiz should populate the moji from the moji list at random
+    Quiz will populate the answers with
+        1 corerct sound
+        3 randomly selected wrong sounds
+
+After 3 rounds it is complete
+Display Answers correct out of total
+*/
+
+
+class Kana {
+ constructor(moji, sound){
+    this.moji = moji;
+    this.sound = sound;
+ }
+    
+test(){
+    return `${this.moji} and ${this.sound}`
+    }
+};
+
+hiraA = new Kana("あ", "a");
+hiraI = new Kana("い", "i");
+hiraU = new Kana("う", "u");
+hiraE = new Kana("え", "e");
+hiraO = new Kana("お", "o");
+
+console.log(hiraI.test())
 
